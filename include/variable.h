82bf20f5c8839e4ba218bb29296408ebfef5be9f
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-05-06 13:13:03 +0000

    doc update

--- include/variable.h
+++ include/variable.h
@@ -24,7 +24,11 @@ typedef struct {
 
 VariableManager *variable_manager_new();
 
+/** @cond
+* Global variables. Used by internal functions.
+*/
 extern VariableManager* global_variables;
+/** @endcond */
 
 void variable_set_value(VariableManager* variables, const char* name, const char* value);
 void variable_set_value_read_only(VariableManager* variables, const char* name, const char* value);
