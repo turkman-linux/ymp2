6d1918ffcf5dcc5c191cfc2f97b631e645c439ee
sulincix <parduscix@yandex.ru>
2025-05-06 22:29:39 +0000

    string utils

---
+++ include/utils/string.h
@@ -0,0 +1,134 @@
+/**
+ * @file string.h
+ * @brief Custom string manipulation functions and macros.
+ *
+ * This header file provides a collection of functions for string manipulation,
+ * including string comparison, length calculation, joining strings, and URL encoding/decoding.
+ * It also includes macros for character classification.
+ */
+
+#ifndef _string_h
+#define _string_h
+
+#include <stddef.h>
+
+/**
+ * @brief Compares two strings for equality.
+ *
+ * @param str1 The first string to compare.
+ * @param str2 The second string to compare.
+ * @return 1 if the strings are equal, 0 otherwise.
+ */
+int iseq(char* str1, char* str2);
+
+/**
+ * @brief Calculates the length of a string.
+ *
+ * @param str The string whose length is to be calculated.
+ * @return The length of the string.
+ */
+size_t sstrlen(const char* str);
+
+/**
+ * @brief Counts the number of tab characters in a given data string.
+ *
+ * @param data The string to search for tab characters.
+ * @return The count of tab characters in the string.
+ */
+long count_tab(char* data);
+
+/**
+ * @brief Joins an array of strings into a single string with a specified delimiter.
+ *
+ * @param f The delimiter to use for joining.
+ * @param array The array of strings to join.
+ * @param length The number of strings in the array.
+ * @return A new string containing the joined strings.
+ */
+char* join(const char* f, char** array, int length);
+
+/**
+ * @brief Concatenates two strings.
+ *
+ * @param str1 The first string.
+ * @param str2 The second string.
+ * @return A new string that is the concatenation of str1 and str2.
+ */
+char* str_add(char* str1, char* str2);
+
+/**
+ * @brief Trims whitespace from the beginning and end of a string.
+ *
+ * @param data The string to trim.
+ * @return A new string with whitespace removed from both ends.
+ */
+char* trim(char* data);
+
+/**
+ * @brief Converts an integer to a string.
+ *
+ * @param num The integer to convert.
+ * @return A string representation of the integer.
+ */
+char* int_to_string(int num);
+
+/**
+ * @brief Decodes a URL-encoded string.
+ *
+ * @param input The URL-encoded string to decode.
+ * @return A new string containing the decoded result.
+ */
+char* url_decode(const char *input);
+
+/**
+ * @brief Encodes a string for use in a URL.
+ *
+ * @param input The string to encode.
+ * @return A new string containing the URL-encoded result.
+ */
+char* url_encode(const char *input);
+
+/**
+ * @brief Checks if a string ends with a specified suffix.
+ *
+ * @param data The string to check.
+ * @param f The suffix to check for.
+ * @return 1 if data ends with f, 0 otherwise.
+ */
+int endswith(const char* data, const char* f);
+
+/**
+ * @brief Checks if a string starts with a specified prefix.
+ *
+ * @param data The string to check.
+ * @param f The prefix to check for.
+ * @return 1 if data starts with f, 0 otherwise.
+ */
+int startswith(const char* data, const char* f);
+
+/**
+ * @brief Builds a formatted string using variable arguments.
+ *
+ * @param format The format string.
+ * @param ... The values to format into the string.
+ * @return A new formatted string.
+ */
+char* build_string(char* format, ...);
+
+// Macros
+
+/** @cond */
+#define isalnum_c(A) \
+    (A >= 'A' && A <= 'Z') || \
+    (A >= 'a' && A <= 'z') || \
+    (A >= '0' && A <= '9') || \
+    A == '-' || A == '_' || A == '.' || A == '~'
+
+#define isHexDigit(A) \
+    (A >= '0' && A <= '9') || \
+    (A >= 'A' && A <= 'F') || \
+    (A >= 'a' && A <= 'f')
+
+/** @endcond */
+#endif // _string_h
+
---
+++ src/utils/string.c
@@ -0,0 +1,228 @@
+#ifndef _string
+#define _string
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+#include <utils/string.h>
+
+visible int iseq(char* str1, char* str2){
+    return strcmp(str1,str2) == 0;
+}
+
+visible size_t sstrlen(const char* str){
+    size_t ret = 0;
+    while(str && str[0] != '\0'){
+        str++;
+        ret++;
+    }
+    return ret;
+}
+
+
+visible long count_tab(char* data){
+    int cnt = 0;
+    while (*data == ' ') {
+        cnt++;
+        data++;
+    }
+    return cnt;
+}
+
+visible char* join(const char* f, char** array, int length){
+    (void) length;
+    int i = 0;
+    int len = 0;
+    /* find output size */
+    while(array[i]){
+        len += sstrlen(array[i]) + sstrlen(f);
+        i++;
+    }
+    /* allocate memory */
+    char* ret = calloc(len+1, sizeof(char));
+    strcpy(ret,"");
+    /* copy item len and reset value */
+    int cnt = i;
+    i = 0;
+    /* copy items */
+    while(array[i]){
+        strcat(ret,array[i]);
+        if(i<cnt-1){
+            strcat(ret,f);
+        }
+	i++;
+    }
+    return ret;
+}
+
+visible char* str_add(char* str1, char* str2){
+    char* ret = calloc( (sstrlen(str1)+sstrlen(str2)+1),sizeof(char) );
+    strcpy(ret,str1);
+    strcat(ret,str2);
+    return ret;
+}
+
+visible char* trim(char* data) {
+    int i=0;
+    int j=0;
+    int cnt=0;
+    int len = sstrlen(data);
+    char* str = calloc(len+1, sizeof(char));
+    strcpy(str,data);
+    cnt = count_tab (data);
+    j=cnt-1;
+    for(i=0; i<len; i++) {
+        j += 1;
+        if(j >= len || str[i] == '\0') {
+            str[i] = '\0';
+            break;
+        }
+        str[i] = data[j];
+        if(str[i] == '\n'){
+            j += cnt;
+
+        }
+    }
+    return str;
+}
+
+visible char* int_to_string(int num){
+    char *ret = calloc(((sizeof(num) - 1) / 3 + 2), sizeof(char));
+    sprintf(ret, "%d", num);
+    return ret;
+}
+
+/* Function to perform URL decoding */
+visible char* url_decode(const char *input) {
+    int cnt = 0;
+    int i;
+    for (i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '%'){
+           if (isHexDigit(input[i + 1])){
+               if (isHexDigit(input[i + 2])) {
+                   /* Skip '%', and the next two characters (assuming they are valid hexadecimal digits) */
+                   i += 2;
+                   cnt++;
+                }
+            }
+        } else {
+            cnt++;
+        }
+    }
+
+    /* +1 for null-terminator */
+    char *output = (char *)calloc((cnt + 1), sizeof(char));
+
+    if (output == NULL) {
+        perror("Memory allocation failed\n");
+        return (char*) input;
+    }
+
+    int j = 0;
+    for (i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '%') {
+            if (isHexDigit(input[i + 1])){
+                if (isHexDigit(input[i + 2])) {
+                    char hex[3] = {input[i + 1], input[i + 2], '\0'};
+                    output[j++] = (char)strtol(hex, NULL, 16);
+                    /* Skip '%', and the next two characters */
+                    i += 2;
+                }
+            }
+        } else {
+            output[j++] = input[i];
+        }
+    }
+    /* Null-terminate the output string */
+    output[j] = '\0';
+
+    return output;
+}
+
+/* Function to perform URL encoding */
+visible char* url_encode(const char *input) {
+    int cnt = 0;
+    int i;
+    for (i = 0; input[i] != '\0'; i++) {
+        if (!isalnum_c(input[i])) {
+            /* Two characters for % and the hexadecimal digit */
+            cnt += 2;
+        } else {
+            cnt++;
+        }
+    }
+
+    /* +1 for null-terminator */
+    char *output = (char *)malloc((cnt + 1) * sizeof(char));
+
+    if (output == NULL) {
+        perror( "Memory allocation failed\n");
+        return (char*) input;
+    }
+
+    int j = 0;
+    for (i = 0; input[i] != '\0'; i++) {
+        if (isalnum_c(input[i])) {
+            output[j++] = input[i];
+        } else {
+            sprintf(output + j, "%%%02X", (unsigned char)input[i]);
+            /* Move to the next position in the output string */
+            j += 3;
+        }
+    }
+    /* Null-terminate the output string */
+    output[j] = '\0';
+
+    return output;
+}
+
+
+visible int endswith(const char* data, const char* f) {
+    int i = sstrlen(data);
+    int j = sstrlen(f);
+
+    if (i < j) {
+        return 0;
+    }
+
+    return strcmp(data + i - j, f) == 0;
+}
+
+visible int startswith(const char* data, const char* f) {
+    int i = sstrlen(data);
+    int j = sstrlen(f);
+
+    if (i < j) {
+        return 0;
+    }
+
+    return strncmp(data, f, j) == 0;
+}
+
+
+visible char* build_string(char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    /* Determine the size needed for the string */
+    int size = vsnprintf(NULL, 0, format, args) + 1;
+    va_end(args);
+
+    /* Allocate memory for the string */
+    char* result = (char*)malloc(size);
+    if (result == NULL) {
+        return "";
+    }
+
+    /* Format the string */
+    va_start(args, format);
+    vsnprintf(result, size, format, args);
+    va_end(args);
+
+    return result;
+}
+
+#endif
