40bb813979b1bcf230794053a3782bbcec6655d3
busra <busra.cagliyan@pardus.org.tr>
2025-05-08 23:08:07 +0300

    add sha1 calculation

---
+++ src/utils/hash.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // For system calls write, read e close
+#include <fcntl.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <utils/file.h>
+#define BUFFER_SIZE 1024
+#define OPENSSL_API_COMPAT
+
+char *calculate_sha1(const char *path) {
+    unsigned char buffer[BUFFER_SIZE];
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+    
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+    md = EVP_sha1();
+    mdctx = EVP_MD_CTX_create();
+    
+    ssize_t byte = 0;
+
+    int fd = open(path, O_RDONLY);
+    EVP_DigestInit_ex(mdctx, md, NULL);
+
+    while ((byte = read(fd, buffer, sizeof(buffer))) != 0) {
+        EVP_DigestUpdate(mdctx, buffer, byte);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    EVP_DigestFinal_ex(mdctx, digest, &md_len);
+    EVP_MD_CTX_destroy(mdctx);
+    close(fd);
+    EVP_cleanup();
+
+    return strdup((char*)digest);
+}
